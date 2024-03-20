51925970b3847c04407f439a98c6c2ceecc295d5
Sambat Das <revosambat123@gmail.com>
2024-03-20 19:33:38 +0530

    Icon change for time in Country Info

--- src/containers/weatherDetails/components/CountryInfo.jsx
+++ src/containers/weatherDetails/components/CountryInfo.jsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import { WeatherContext } from '../../../router/Router'
 import moment from 'moment-timezone'
-import LocationOnTwoToneIcon from '@mui/icons-material/LocationOnTwoTone';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const CityHeading = styled(Typography)`
     color: #e3f2fd;
@@ -18,16 +18,12 @@ const CityHeading = styled(Typography)`
 `
 const CountryInfo = () => {
     const weatherData = useContext(WeatherContext)
-    console.log(weatherData)
     return (
         <Stack sx={{ paddingTop: '5%' }} justifyContent='space-between' direction='row'>
             <CityHeading level='h2'>{weatherData.city_name}, {weatherData.country_code}</CityHeading>
             <Box>
                 <CityHeading sx={{ textAlign: 'right' }}>
-                    <LocationOnTwoToneIcon sx={{ fontSize: '50px' }} />
-                </CityHeading>
-                <CityHeading>
-
+                    <AccessTimeFilledIcon sx={{ fontSize: '50px' }} />
                 </CityHeading>
                 <CityHeading level='h3'>{moment().tz(weatherData.timezone)?.format('hh:mm A')}</CityHeading>
             </Box>
