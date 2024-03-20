7ab9b7e2a50429f8383f0f1e91a2a94f511005f5
Sambat Das <revosambat123@gmail.com>
2024-03-20 19:35:21 +0530

    constant field description added for decoding the weather api response key

---
+++ src/contants/fieldDescription.js
@@ -0,0 +1,203 @@
+export const fieldDesc = {
+    lat: {
+        name: 'Latitude',
+        unit: '(Degree)'
+    },
+    lon: {
+        name: 'Longitude',
+        unit: '(Degree)'
+    },
+    sunrise: {
+        name: 'Sunrise time UTC',
+        unit: '(HH:MM)'
+    },
+    clouds: {
+        name: '[Satellite based] average cloud coverage',
+        unit: '(%)'
+    },
+    date: {
+        name: 'Selected Date',
+        unit: 'YYYY-MM-DD',
+    },
+    sunset: {
+        name: 'Sunset time UTC',
+        unit: '(HH:MM)'
+    },
+    timezone: {
+        name: 'Local IANA Timezone'
+    },
+    station: {
+        name: '[DEPRECATED] Nearest reporting station ID'
+    },
+    sources: {
+        name: 'List of data sources used in response'
+    },
+    ob_time: {
+        name: 'Last observation time',
+        unit: '(YYYY-MM-DD HH:MM)'
+    },
+    datetime: {
+        name: '[DEPRECATED] Current cycle hour',
+        unit: '(YYYY-MM-DD:HH)'
+    },
+    ts: {
+        name: 'Last observation time',
+        unit: '(Unix timestamp)'
+    },
+    city_name: {
+        name: 'City name'
+    },
+    country_code: {
+        name: 'Country abbreviation'
+    },
+    state_code: {
+        name: 'State abbreviation/code'
+    },
+    pres: {
+        name: 'Pressure',
+        unit: '(mb)'
+    },
+    slp: {
+        name: 'Sea level pressure',
+        unit: '(mb)'
+    },
+    wind_spd: {
+        name: 'Wind speed',
+        unit: '(Default m/s)'
+    },
+    max_wind_spd: {
+        name: 'Maximum 2 minute wind speed',
+        unit: '(m/s)'
+    },
+    gust: {
+        name: 'Wind gust speed',
+        unit: '(Default m/s)'
+    },
+    wind_dir: {
+        name: 'Wind direction',
+        unit: '(degrees)'
+    },
+    wind_cdir: {
+        name: 'Abbreviated wind direction'
+    },
+    wind_cdir_full: {
+        name: 'Verbal wind direction'
+    },
+    temp: {
+        name: 'Temperature',
+        unit: '(default Celsius)'
+    },
+    app_temp: {
+        name: 'Apparent/"Feels Like" temperature',
+        unit: '(default Celsius)'
+    },
+    rh: {
+        name: 'Relative humidity',
+        unit: '(%)'
+    },
+    dewpt: {
+        name: 'Dew point',
+        unit: '(default Celsius)'
+    },
+    clouds: {
+        name: 'Cloud coverage',
+        unit: '(%)'
+    },
+    pod: {
+        name: 'Part of the day'
+    },
+    vis: {
+        name: 'Visibility',
+        unit: '(default KM)'
+    },
+    max_wind_dir: {
+        name: 'Direction of maximum 2 minute wind gust',
+        unit: '(degrees)'
+    },
+
+    precip: {
+        name: 'Liquid equivalent precipitation rate',
+        unit: '(default mm/hr)'
+    },
+    snow: {
+        name: 'Snowfall',
+        unit: '(default mm/hr)'
+    },
+    uv: {
+        name: 'UV Index',
+        unit: '(0-11+)'
+    },
+    aqi: {
+        name: 'Air Quality Index',
+        unit: '[US - EPA standard 0 - +500]'
+    },
+    dhi: {
+        name: 'Diffuse horizontal solar irradiance',
+        unit: '(W/m^2) [Clear Sky]'
+    },
+    dni: {
+        name: 'Direct normal solar irradiance',
+        unit: '(W/m^2) [Clear Sky]'
+    },
+    ghi: {
+        name: 'Global horizontal solar irradiance',
+        unit: '(W/m^2) [Clear Sky]'
+    },
+    solar_rad: {
+        name: 'Estimated Solar Radiation',
+        unit: '(W/m^2)'
+    },
+    elev_angle: {
+        name: 'Solar elevation angle',
+        unit: '(degrees)'
+    },
+    h_angle: {
+        name: '[DEPRECATED] Solar hour angle',
+        unit: '(degrees)'
+    },
+    revision_status: {
+        name: 'Data revision status - interim (subject to revisions) or final',
+        unit: ''
+    },
+    t_solar_rad: {
+        name: 'Total solar radiation',
+        unit: '(W/M^2)'
+    },
+    t_ghi: {
+        name: 'Day total global horizontal solar irradiance [Clear Sky]',
+        unit: '(W/m^2) '
+    },
+    t_dhi: {
+        name: 'Day total diffuse horizontal solar irradiance',
+        unit: '(W/m^2)'
+    },
+    t_dni: {
+        name: 'Day total direct normal solar irradiance',
+        unit: '(W/m^2)'
+    },
+    temp_wetbulb: {
+        name: 'Average wet bulb temperature over period',
+        unit: '(default Celsius)'
+    },
+    cdd: {
+        name: 'Total number of cooling degree days relative to threshold temperature.',
+        unit: ''
+    },
+    hdd: {
+        name: 'Total number of heating degree days relative to threshold temperature.',
+        unit: ''
+    },
+    sun_hours: {
+        name: 'Total peak sun hours over period.',
+        unit: ''
+    },
+    timestamp_local: {
+        name: 'Timestamp at local time.',
+        unit: ''
+    },
+    timestamp_utc: {
+        name: 'Timestamp at UTC time.',
+        unit: ''
+    }
+
+};
